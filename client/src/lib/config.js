09e39668364194a1e036aaf854b77a995fa3637d
Naitik Prateek <naitikprateek347@gmailcom>
2025-12-16 18:13:16 +0530

    fix: correct url issues for Netlify frontend

--- client/src/lib/config.js
+++ client/src/lib/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://stylekat.onrender.com/"
-export const SOCKET_URL = "https://stylekat.onrender.com/";
+export const API_URL = "https://stylekat.onrender.com"
+export const SOCKET_URL = "https://stylekat.onrender.com";
