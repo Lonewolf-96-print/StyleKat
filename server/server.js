9ed4847171b75d9cf090c8e92bf1de24c6971b9c
Naitik Prateek <naitikprateek347@gmailcom>
2025-12-18 20:40:45 +0530

    fix Frontend URL for Netlify frontend

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ import Staff from "./model/Staff.model.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
-  "https://astonishing-kitten-35c5aa.netlify.app"
+  "https://teal-conkies-c2ecca.netlify.app"
 ];
 const app = express();
 
 app.use(
   cors({
-    origin: "https://astonishing-kitten-35c5aa.netlify.app",
+    origin: "https://teal-conkies-c2ecca.netlify.app",
     credentials: true,
   })
 );
@@ -51,7 +51,7 @@ function getTodayString(offsetDays = 0) {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://astonishing-kitten-35c5aa.netlify.app",
+    origin: "https://teal-conkies-c2ecca.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
