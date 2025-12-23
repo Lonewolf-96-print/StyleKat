578b89de822916127620ebee3bb93b668aa41a90
Naitik Prateek <naitikprateek347@gmailcom>
2025-12-24 01:12:56 +0530

    fix frontend URL

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ import Staff from "./model/Staff.model.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
-  "https://style-hthhhv24k-naitiks-projects-15e2f418.vercel.app"
+  "https://extraordinary-mooncake-e927f9.netlify.app/"
 ];
 const app = express();
 
 app.use(
   cors({
-    origin: "https://style-hthhhv24k-naitiks-projects-15e2f418.vercel.app",
+    origin: "https://extraordinary-mooncake-e927f9.netlify.app",
     credentials: true,
   })
 );
@@ -51,7 +51,7 @@ function getTodayString(offsetDays = 0) {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://style-hthhhv24k-naitiks-projects-15e2f418.vercel.app",
+    origin: "https://extraordinary-mooncake-e927f9.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
